c026bb42676e1062f454a8650412fbe7f3579bde
benevenutoIsa <142352312+benevenutoIsa@users.noreply.github.com>
2024-11-18 20:38:01 -0300

    Update ArvoreBin.c

--- Code/ArvoreBin.c
+++ Code/ArvoreBin.c
@@ -1,49 +1,56 @@
-#include  <stdio.h>
-#include <stdlib.h>
-#include "ArvoreBin.h"
-#include "Lib.h"
+#include  <stdio.h>  
+#include <stdlib.h> 
+#include "ArvoreBin.h"  
+#include "Lib.h"  
 
+// cria um nó da árvore com um paciente
 EArvore *criaEArvore(Paciente *paciente){
-    EArvore *celula = malloc(sizeof(EArvore));
-    celula->paciente = paciente;
-    celula->direito = NULL;
-    celula->esquerdo = NULL;
+    EArvore *celula = malloc(sizeof(EArvore));  // aloca memória para a célula
+    celula->paciente = paciente;  // associa o paciente à célula
+    celula->direito = NULL;  // inicializa o ponteiro direito como NULL
+    celula->esquerdo = NULL;  // inicializa o ponteiro esquerdo como NULL
 
-    return celula;
+    return celula;  // retorna a célula criada
 }
 
+// cria a árvore binária e inicializa suas variáveis
 Arvore *criaArvore(){
-    Arvore *arvore = malloc(sizeof(Arvore));
-    arvore->altura = 0;
-    arvore->quantidade = 0;
-    arvore->raiz = NULL;
+    Arvore *arvore = malloc(sizeof(Arvore));  // aloca memória para a árvore
+    arvore->altura = 0;  // define a altura inicial como 0
+    arvore->quantidade = 0;  // define a quantidade de nós como 0
+    arvore->raiz = NULL;  // define a raiz como NULL
 
-    return arvore;
+    return arvore;  // retorna a árvore criada
 }
 
+// exibe os dados do paciente de um nó da árvore
 void visitarEArvore(EArvore *celula){
-    if(celula != NULL){
-        Paciente *paciente = celula->paciente;
+    if(celula != NULL){  // verifica se a célula não é NULL
+        Paciente *paciente = celula->paciente;  // obtém o paciente
+        // imprime os dados do paciente
         printf("Nome: %s\n Idade: %d\n RG: %s\n Data de Cadastro: %02d/%02d/%d\n",
-               paciente->nome, paciente->idade,paciente->RG, 
+               paciente->nome, paciente->idade, paciente->RG, 
                paciente->cadastro->dia, paciente->cadastro->mes, paciente->cadastro->ano);
     }
 }
 
+// percorre a árvore em ordem e exibe os dados de todos os pacientes
 void mostrarEmOrdem(EArvore *raiz){
-     if (raiz != NULL) {
-        mostrarEmOrdem(raiz->esquerdo);
-        visitarEArvore(raiz);
-        mostrarEmOrdem(raiz->direito);
+    if (raiz != NULL) {
+        mostrarEmOrdem(raiz->esquerdo);  // percorre a subárvore esquerda
+        visitarEArvore(raiz);  // visita o nó atual
+        mostrarEmOrdem(raiz->direito);  // percorre a subárvore direita
     }
 }
 
+// encontra a posição adequada para inserir um novo nó na árvore, com base no tipo de ordenação
 EArvore *balanceamento(Arvore *arvore, Paciente *paciente, int tipoArvore){
-    EArvore *anterior = NULL;
-    EArvore *atual = arvore->raiz;
+    EArvore *anterior = NULL;  // armazena o nó anterior
+    EArvore *atual = arvore->raiz;  // começa da raiz
 
-    while(atual != NULL){
-        if(tipoArvore == 1){
+    while(atual != NULL){  // percorre a árvore até encontrar a posição
+        // dependendo do tipo da árvore, a comparação é feita com diferentes critérios (idade, dia, mês, ano)
+        if(tipoArvore == 1){  // ordenação por idade
             if(paciente->idade <= atual->paciente->idade){
                 anterior = atual;
                 atual = atual->esquerdo;
@@ -51,7 +58,7 @@ EArvore *balanceamento(Arvore *arvore, Paciente *paciente, int tipoArvore){
                 anterior = atual;
                 atual = atual->direito;
             }
-        } else if(tipoArvore == 2){
+        } else if(tipoArvore == 2){  // ordenação por dia de cadastro
             if(paciente->cadastro->dia <= atual->paciente->cadastro->dia){
                 anterior = atual;
                 atual = atual->esquerdo;
@@ -59,7 +66,7 @@ EArvore *balanceamento(Arvore *arvore, Paciente *paciente, int tipoArvore){
                 anterior = atual;
                 atual = atual->direito;
             }
-        } else if(tipoArvore == 3){
+        } else if(tipoArvore == 3){  // ordenação por mês de cadastro
             if(paciente->cadastro->mes <= atual->paciente->cadastro->mes){
                 anterior = atual;
                 atual = atual->esquerdo;
@@ -67,7 +74,7 @@ EArvore *balanceamento(Arvore *arvore, Paciente *paciente, int tipoArvore){
                 anterior = atual;
                 atual = atual->direito;
             }
-        } else if(tipoArvore == 4){
+        } else if(tipoArvore == 4){  // ordenação por ano de cadastro
             if(paciente->cadastro->ano <= atual->paciente->cadastro->ano){
                 anterior = atual;
                 atual = atual->esquerdo;
@@ -77,42 +84,42 @@ EArvore *balanceamento(Arvore *arvore, Paciente *paciente, int tipoArvore){
             }
         }
     }
-    return anterior;
+    return anterior;  // retorna o nó onde o paciente deve ser inserido
 }
 
+// insere um novo paciente na árvore, balanceando a árvore de acordo com o critério especificado
 void inserirNaArvore(Arvore *arvore, Paciente *paciente, int tipoArvore){
-    EArvore *novo = criaEArvore(paciente);
-    if(arvore->raiz == NULL){
-        printf("F");
-        arvore->raiz = novo;
+    EArvore *novo = criaEArvore(paciente);  // cria o novo nó
+    if(arvore->raiz == NULL){  // se a árvore estiver vazia
+        arvore->raiz = novo;  // a nova célula se torna a raiz
     } else {
-        EArvore *posicao = balanceamento(arvore, paciente, tipoArvore);
-        if (tipoArvore == 1) {
+        EArvore *posicao = balanceamento(arvore, paciente, tipoArvore);  // encontra a posição correta para o novo nó
+        // insere o novo nó dependendo do critério de ordenação
+        if (tipoArvore == 1) {  // ordenação por idade
             if (paciente->idade <= posicao->paciente->idade) {
-                posicao->esquerdo = novo;
+                posicao->esquerdo = novo;  // insere à esquerda
             } else {
-                posicao->direito = novo;
+                posicao->direito = novo;  // insere à direita
             }
-        } else if (tipoArvore == 2) {
+        } else if (tipoArvore == 2) {  // ordenação por dia de cadastro
             if (paciente->cadastro->dia <= posicao->paciente->cadastro->dia) {
-                posicao->esquerdo = novo;
+                posicao->esquerdo = novo;  // insere à esquerda
             } else {
-                posicao->direito = novo;
+                posicao->direito = novo;  // insere à direita
             }
-        } else if (tipoArvore == 3) {
+        } else if (tipoArvore == 3) {  // ordenação por mês de cadastro
             if (paciente->cadastro->mes <= posicao->paciente->cadastro->mes) {
-                posicao->esquerdo = novo;
+                posicao->esquerdo = novo;  // insere à esquerda
             } else {
-                posicao->direito = novo;
+                posicao->direito = novo;  // insere à direita
             }
-        } else if (tipoArvore == 4) {
+        } else if (tipoArvore == 4) {  // ordenação por ano de cadastro
             if (paciente->cadastro->ano <= posicao->paciente->cadastro->ano) {
-                posicao->esquerdo = novo;
+                posicao->esquerdo = novo;  // insere à esquerda
             } else {
-                posicao->direito = novo;
-                printf("F");
+                posicao->direito = novo;  // insere à direita
             }
         }
     }
-    arvore->quantidade++;
+    arvore->quantidade++;  // incrementa a quantidade de pacientes na árvore
 }
